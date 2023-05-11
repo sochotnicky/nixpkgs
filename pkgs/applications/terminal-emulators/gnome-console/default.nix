e54227b0df976663ae41962d294aab19f526be45
Robert Hensing <robert@roberthensing.nl>
2023-05-11 15:07:45 +0200

    Revert "nixosTests: Resolve alias kgx = gnome-console"
    This reverts commit a486968a6eeed10d157cb537ea58a3bacb187711.

--- pkgs/applications/terminal-emulators/gnome-console/default.nix
+++ pkgs/applications/terminal-emulators/gnome-console/default.nix
@@ -48,7 +48,7 @@ stdenv.mkDerivation rec {
     };
   };
 
-  passthru.tests.test = nixosTests.terminal-emulators.gnome-console;
+  passthru.tests.test = nixosTests.terminal-emulators.kgx;
 
   meta = with lib; {
     description = "Simple user-friendly terminal emulator for the GNOME desktop";
